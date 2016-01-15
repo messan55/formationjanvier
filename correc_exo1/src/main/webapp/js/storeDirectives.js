88ebe82d02d3098b9ed15478e15cbe02312c2cc0
Formateur <Formateur@PM8-Instruct>
2016-01-15 17:11:28 +0100

    correction fonction dans directive

--- correc_exo1/src/main/webapp/js/storeDirectives.js
+++ correc_exo1/src/main/webapp/js/storeDirectives.js
@@ -4,7 +4,16 @@ angular.module("storeDirectives", [])
              restrict: "E",
              templateUrl: "components/productCount.html",
              controller: function($scope) {
-                 console.log("demarrage scope directive");
+                 //console.log("demarrage scope directive");
+                 $scope.inStockProducts = function() {
+                     var count = 0;
+                     angular.forEach($scope.produits, function(p) {
+                         if (p.stock > 0) {
+                             count++;
+                         }
+                     });
+                     return count;
+                 }
              }
            }; 
         });

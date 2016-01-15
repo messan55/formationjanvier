b9d36957321e599f80dd7de6318599c7fc6a7060
Formateur <Formateur@PM8-Instruct>
2016-01-15 14:58:06 +0100

    ecriture forulaire de creation/edition de produit

--- correc_exo1/src/main/webapp/js/angularapp.js
+++ correc_exo1/src/main/webapp/js/angularapp.js
@@ -8,9 +8,18 @@ var todoApp = angular.module("storeApp", []);
 // le scope est automatiquement injecté par angular
 todoApp.controller("StoreCtrl", function($scope, $http) {
    $scope.produits = [];
+   $scope.currentView = 'views/productsList.html';
    
    $http.get('rest/produit').then(function (response) {
        $scope.produits = response.data.produits;
    });
+   
+   $scope.navigateTo = function(view) {
+       $scope.currentView = view;
+   };
  
+   $scope.addProduct = function (product) {
+       console.log(product.nom + " - " + product.prix);
+       $scope.navigateTo('views/productsList.html'); 
+   };
 });

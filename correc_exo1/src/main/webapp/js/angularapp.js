7fe4fb2ac7adcf2c75eb6a41d8c94e028082ff7c
Formateur <Formateur@PM8-Instruct>
2016-01-18 10:14:08 +0100

    ajout des routes angular

--- correc_exo1/src/main/webapp/js/angularapp.js
+++ correc_exo1/src/main/webapp/js/angularapp.js
@@ -2,8 +2,21 @@
 // cette ligne creer un nouveau module angular
 // le tableau en deuxieme argument contient les
 // dépendances a injecter dans ce module
-var storeApp = angular.module("storeApp", ['productsModule', 'storeDirectives']);
+var storeApp = angular.module("storeApp", ['productsModule', 'storeDirectives', 'ngRoute']);
 
+
+storeApp.config(function($routeProvider) {
+   $routeProvider.when("/products", {
+      templateUrl: "/views/products.html" 
+   });
+   $routeProvider.when("/admin", {
+       templateUrl: "/views/admin.html"
+   });
+   $routeProvider.otherwise( {
+      templateUrl: "/views/products.html" 
+   });
+    
+});
 // creation d'un controlleur "toDoCtrl" dans notre module
 // le scope est automatiquement injecté par angular
 storeApp.controller("StoreCtrl", function($scope, $http) {

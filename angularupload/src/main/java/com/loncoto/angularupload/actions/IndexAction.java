283efad035c359f3f52ce95f971cc173be87dc6e
Formateur <Formateur@PM8-Instruct>
2016-01-18 12:23:19 +0100

    ajout de l'action struts2 gérant l'upoad d'image

--- angularupload/src/main/java/com/loncoto/angularupload/actions/IndexAction.java
+++ angularupload/src/main/java/com/loncoto/angularupload/actions/IndexAction.java
@@ -1,19 +1,35 @@
 package com.loncoto.angularupload.actions;
 
+import java.io.File;
 import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 public class IndexAction extends ActionSupport {
 	
+	private static final Logger log = LogManager.getLogger(IndexAction.class);
 	
 	private static final long serialVersionUID = 1L;
 
+	private File upload;
+	private String uploadContentType;
+	private String uploadFileName;
 	
-
+	public void setUpload(File upload) {this.upload = upload;}
+	public void setUploadContentType(String uploadContentType) {this.uploadContentType = uploadContentType;}
+	public void setUploadFileName(String uploadFileName) {this.uploadFileName = uploadFileName;}
 
 	public String index() {
 		return SUCCESS;
 	}
 
+	public String uploadImage() {
+		log.info("reception upload: content type = " + uploadContentType);
+		log.info("reception upload: FileName = " + uploadFileName);
+		log.info("reception upload: file = " + upload.getAbsolutePath());
+		return SUCCESS;
+	}
 }

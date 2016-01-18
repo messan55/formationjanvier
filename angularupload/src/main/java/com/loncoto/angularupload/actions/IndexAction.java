d5df37296fd3ec9d535e1600ebfbb02fb96e1325
Formateur <Formateur@PM8-Instruct>
2016-01-18 10:38:11 +0100

    creation projet angular upload

---
+++ angularupload/src/main/java/com/loncoto/angularupload/actions/IndexAction.java
@@ -0,0 +1,19 @@
+package com.loncoto.angularupload.actions;
+
+import java.util.Date;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport {
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	
+
+
+	public String index() {
+		return SUCCESS;
+	}
+
+}

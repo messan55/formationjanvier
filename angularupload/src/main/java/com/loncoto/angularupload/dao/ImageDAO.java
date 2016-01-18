1cf4341f1e7acc9dcf1ce5f6a731ce4405098cb0
Formateur <Formateur@PM8-Instruct>
2016-01-18 12:06:55 +0100

    injection du fileStorage dans le dao Image

--- angularupload/src/main/java/com/loncoto/angularupload/dao/ImageDAO.java
+++ angularupload/src/main/java/com/loncoto/angularupload/dao/ImageDAO.java
@@ -1,14 +1,24 @@
 package com.loncoto.angularupload.dao;
 
+import java.io.File;
 import java.util.List;
 
 import com.loncoto.angularupload.metier.Image;
+import com.loncoto.angularupload.utils.FileStoreManager;
 
 public class ImageDAO extends GenericDAO<Image> implements IImageDAO {
+
+	private FileStoreManager fileStoreManager;
+	public FileStoreManager getFileStoreManager() {return fileStoreManager;}
+	public void setFileStoreManager(FileStoreManager fileStoreManager) {this.fileStoreManager = fileStoreManager;}
+
 
 	public ImageDAO() {
 		super(Image.class);
 	}
 	
-
+	public boolean saveImageFile(int id, File f) {
+		return getFileStoreManager().saveFile("Image", id, f);
+	}
+	
 }

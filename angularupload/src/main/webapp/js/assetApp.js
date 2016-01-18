4e7bdd49405b2a0712ef9264a6a16104891b70c2
Formateur <Formateur@PM8-Instruct>
2016-01-18 15:06:01 +0100

    ajout de la logique pour le drag and drop des fichiers

---
+++ angularupload/src/main/webapp/js/assetApp.js
@@ -0,0 +1,13 @@
+var app = angular.module('assetApp', ['ngFileUpload']);
+
+app.controller('imageCtrl', ['$scope', 'Upload', '$timeout', function($scope, Upload, $timeout) {
+    // je surveille la variable files du scope
+    // le callback sera rappelé a sa mise à jour
+    $scope.$watch('files', function() {
+        console.log($scope.files.length);
+        for (var idx in $scope.files) {
+            console.log($scope.files[idx].name);
+        }
+    });
+    
+}]);

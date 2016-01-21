fb4f0cebf765e786c3adf27bf18dc2a8cedba692
Formateur <Formateur@PM8-Instruct>
2016-01-21 12:02:08 +0100

    ajout de stest et correction bug fileStorageManager

--- myFirstTDD/src/main/java/com/loncoto/myFirstTDD/util/FileStoreManager.java
+++ myFirstTDD/src/main/java/com/loncoto/myFirstTDD/util/FileStoreManager.java
@@ -1,6 +1,9 @@
 package com.loncoto.myFirstTDD.util;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public class FileStoreManager {
@@ -21,11 +24,64 @@ public class FileStoreManager {
 	
 	
 	public boolean saveFile(String entityName, int id, File f) {
-		return true;
+		if (storageRoot == null
+			    || !storageRoot.exists()
+			    || !storageRoot.isDirectory()) {
+				System.out.println("stockage de fichier impossible, storageRoot non utilisable");
+				return false;
+			}
+			if (entityName.matches(".*[*:].*")) {
+				//System.out.println("remplacement.....");
+				entityName = entityName.replaceAll("[*]", "-etoile-");
+				entityName = entityName.replaceAll("[:]", "-colonne-");
+				//System.out.println("nouveau nom = " + entityName);
+			}
+			File entityRep = new File(storageRoot.getAbsolutePath() + File.separatorChar + entityName);
+			// si le répertoire de l'entité n'existe pas, le creer
+			if (!entityRep.exists()) {
+				entityRep.mkdirs();
+			}
+			if (!entityRep.isDirectory()) {
+				System.out.println("impossible d'acceder ou de creer le repertoire pour " + entityName);
+				return false;
+			}
+			try {
+				Files.copy(f.toPath(), Paths.get(entityRep.getAbsolutePath(), String.format("FILE_%06d", id))
+												, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+				return true;
+			} catch (IOException e) {
+				System.out.println("erreur" + e);
+			}
+		return false;
 	}
 	
 	public Optional<File> getFile(String entityName, int id) {
-		return Optional.empty();
+		if (storageRoot == null
+			    || !storageRoot.exists()
+			    || !storageRoot.isDirectory()) {
+			System.out.println("stockage de fichier impossible, storageRoot non utilisable");
+				return Optional.empty();
+		}
+		if (entityName.matches(".*[*:].*")) {
+			entityName = entityName.replaceAll("[*]", "-etoile-");
+			entityName = entityName.replaceAll("[:]", "-colonne-");
+		}
+		
+		File entityRep = new File(storageRoot.getAbsolutePath() + File.separatorChar + entityName);
+		// si le répertoire de l'entité n'existe pas, le creer
+		if (!entityRep.isDirectory()) {
+			System.out.println("impossible d'acceder ou de creer le repertoire pour " + entityName);
+			return Optional.empty();
+		}
+		
+		File f = new File(entityRep.getAbsolutePath() + File.separatorChar + String.format("FILE_%06d", id));
+		if (f.exists() && f.isFile()) {
+			return Optional.of(f);
+		}
+		else {
+			System.out.println("fichier inconnu" + f.getAbsolutePath());
+			return Optional.empty();
+		}
 	}
 	
 
--- myFirstTDD/src/test/java/com/loncoto/myFirstTDD/util/FileStoreManagerTest.java
+++ myFirstTDD/src/test/java/com/loncoto/myFirstTDD/util/FileStoreManagerTest.java
@@ -1,7 +1,12 @@
 package com.loncoto.myFirstTDD.util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.Scanner;
 
 import com.loncoto.myFirstTDD.AppTest;
 
@@ -15,7 +20,7 @@ public class FileStoreManagerTest extends TestCase {
 		super("test du file Store Manager");
 	}
 	
-	public void testFileStoreManager()
+	public void testSaveAndGet()
 	{
 		FileStoreManager fsm = new FileStoreManager();
 		
@@ -33,14 +38,88 @@ public class FileStoreManagerTest extends TestCase {
 		
 		
 		fsm.setStorageRoot(new File("c:\\fsm"));
-		
+		/*
 		File f = new File("c:\\partage\\binding-angular.txt");
 		fsm.saveFile("testentite", 4, f);
 		Optional<File> actualValue = fsm.getFile("testentite", 4);
 		assertTrue(actualValue.isPresent());
+		*/
+		try {
+			// getClass().getRessource vous permet de récupérer les ressources
+			// associée a notre classe, ce qui concretement correspond
+			// au fichiers présent dans notre package, c.a.d dans le meme répertoire
+			// que notre classe
+			
+			// quand maven compile un fichier java -> .class -> target/<packagedirectory>
 		
-		
+			File f = new File(getClass().getResource("testFile.txt").toURI());
+			fsm.saveFile("testentite", 4, f);
+			Optional<File> actualValue = fsm.getFile("testentite", 4);
+			assertTrue("le fichier n'a pas été retrouvé",actualValue.isPresent());
+			
+			StringBuilder sb = new StringBuilder();
+			Scanner reader = new Scanner(f);
+			while (reader.hasNext()) {
+				sb.append(reader.next());
+			}
+			
+			StringBuilder sb2 = new StringBuilder();
+			Scanner reader2 = new Scanner(actualValue.get());
+			while (reader2.hasNext()) {
+				sb2.append(reader2.next());
+			}
+			// test que le contenu du fichier ne change pas apres stockage dans le fileStorageManager
+			assertEquals("le contenu du fichier après sauvegarde a changé", sb.toString(), sb2.toString());
+			
+			
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public void testSaveAndGetSPecialPath()
+	{
+		FileStoreManager fsm = new FileStoreManager();
+		
+		
+		
+		
+		fsm.setStorageRoot(new File("c:\\fsm"));
+		try {
+		
+			File f = new File(getClass().getResource("testFile.txt").toURI());
+			fsm.saveFile("4*reviews:", 4, f);
+			Optional<File> actualValue = fsm.getFile("4*reviews:", 4);
+			assertTrue("le fichier n'a pas été retrouvé",actualValue.isPresent());
+			
+			StringBuilder sb = new StringBuilder();
+			Scanner reader = new Scanner(f);
+			while (reader.hasNext()) {
+				sb.append(reader.next());
+			}
+			
+			StringBuilder sb2 = new StringBuilder();
+			Scanner reader2 = new Scanner(actualValue.get());
+			while (reader2.hasNext()) {
+				sb2.append(reader2.next());
+			}
+			// test que le contenu du fichier ne change pas apres stockage dans le fileStorageManager
+			assertEquals("le contenu du fichier après sauvegarde a changé", sb.toString(), sb2.toString());
+			
+			
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	
 	public static Test suite()
 	{
